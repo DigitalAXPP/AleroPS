3d6d97c84a54afe1cbc27fd4355cd0478c924958
DigitalAXPP <a.piepenhagen@gmx.net>
2020-11-19 21:41:14 +0100

    Adding 'get group by ID' to the Get-AleroGroups function.

--- Functions/Groups/Get-AleroGroups.ps1
+++ Functions/Groups/Get-AleroGroups.ps1
@@ -1,33 +1,52 @@
 function Get-AleroGroups {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='BySearch')]
     param (
         [Parameter(
             Mandatory,
             ValueFromPipelineByPropertyName,
+            ParameterSetName='ByGroupId',
+            HelpMessage='Token to authenticate to Alero.'
+        )]
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName,
+            ParameterSetName='BySearch',
             HelpMessage='Token to authenticate to Alero.'
         )]
         [System.Security.SecureString]$Authn,
         
         [Parameter(
+            ParameterSetName='BySearch',
             HelpMessage='The maximum number of entries to return'
         )]
         [int]$Limit = 100,
         
         [Parameter(
+            ParameterSetName='BySearch',
             HelpMessage='The number of entries to skip'
         )]
         [int]$Offset = 0,
         
         [Parameter(
+            ParameterSetName='BySearch',
             HelpMessage='The field in which to perform the search'
         )]
         [ValidateSet('ALL', 'NAME', 'DESCRIPTION')]
         [string]$SearchIn='ALL',
         
         [Parameter(
+            Mandatory,
+            ParameterSetName='BySearch',
             HelpMessage='The string to use in the search'
         )]
-        [string]$Search
+        [SupportsWildcards()]
+        [string]$Search,
+        
+        [Parameter(
+            ParameterSetName='ByGroupId',
+            HelpMessage='The unique ID of the AleroLDAP group'
+        )]
+        [string]$GroupId
     )
     
     begin {
@@ -35,7 +54,15 @@ function Get-AleroGroups {
     }
     
     process {
-        $url = "https://api.alero.io/v2-edge/groups?limit=$Limit&offset=$Offset&searchIn=$SearchIn&searchString=$Search"
+        switch ($PSCmdlet.ParameterSetName) {
+            'BySearch' { 
+                $url = "https://api.alero.io/v2-edge/groups?limit=$Limit&offset=$Offset&searchIn=$SearchIn&searchString=$Search"
+            }
+            'ByGroupId' {
+                $url = "https://api.alero.io/v2-edge/groups/$GroupId"
+            }
+            Default {}
+        }        
         $result = Invoke-RestMethod -Method Get -Uri $url -Authentication Bearer -Token $Authn
     }
     
