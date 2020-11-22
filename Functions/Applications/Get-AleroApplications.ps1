b65b3bcc1728cb620d763246450046d93fa70bfd
DigitalAXPP <a.piepenhagen@gmx.net>
2020-11-22 14:13:01 +0100

    New function: Get-AleroApplications

---
+++ Functions/Applications/Get-AleroApplications.ps1
@@ -0,0 +1,42 @@
+function Get-AleroApplications {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName,
+            HelpMessage='Token to authenticate to Alero.'
+        )]
+        [System.Security.SecureString]$Authn,
+
+        [Parameter(
+            Mandatory,
+            HelpMessage='Enter the Site Id'
+        )]
+        [string]$SiteId,
+        
+        [Parameter(
+            HelpMessage='The maximum number of entries to return'
+        )]
+        [int]$Limit=100,
+
+        
+        [Parameter(
+            HelpMessage='The number of entries to skip'
+        )]
+        [int]$Offset=0
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        $url = "https://api.alero.io/v2-edge/sites/$SiteId/applications?limit=$Limit&offset=$Offset"
+        $result = Invoke-RestMethod -Method Get -Uri $url -Authentication Bearer -Token $Authn
+    }
+    
+    end {
+        Write-Output -InputObject $result
+        Remove-Variable -Name result
+    }
+}
