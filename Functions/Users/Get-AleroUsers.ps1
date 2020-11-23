a18bfada973a374776c52d63443d54f5b0572978
DigitalAXPP <a.piepenhagen@gmx.net>
2020-11-23 21:47:11 +0100

    Adding single search functionality with UserID parameter.

--- Functions/Users/Get-AleroUsers.ps1
+++ Functions/Users/Get-AleroUsers.ps1
@@ -1,27 +1,46 @@
 function Get-AleroUsers {
-    [CmdletBinding()]
+    [CmdletBinding(
+        DefaultParameterSetName='BySearch'
+    )]
     param (
         [Parameter(
             Mandatory,
             ValueFromPipelineByPropertyName,
+            ParameterSetName='BySearch',
+            HelpMessage='Token to authenticate to Alero.'
+        )]
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName,
+            ParameterSetName='ById',
             HelpMessage='Token to authenticate to Alero.'
         )]
         [System.Security.SecureString]$Authn,
 
         [Parameter(
+            ParameterSetName='BySearch',
             HelpMessage='The name of the users to include in the returned list, or part of the name.')]
         [SupportsWildcards()]
         [string[]]$Name,
         
         [Parameter(
+            ParameterSetName='BySearch',
             HelpMessage='The maximum number of entries to return'
         )]
         [int]$Limit=100,
         
         [Parameter(
+            ParameterSetName='BySearch',
             HelpMessage='The number of entries to skip'
         )]
-        [int]$Offset=0
+        [int]$Offset=0,
+        
+        [Parameter(
+            Mandatory,
+            ParameterSetName='ById',
+            HelpMessage='The unique ID of the user'
+        )]
+        [string]$UserId
     )
     
     begin {
@@ -29,7 +48,15 @@ function Get-AleroUsers {
     }
     
     process {
-        $url = "https://api.alero.io/v2-edge/users/?limit=$Limit&name=$Name&offset=$Offset"
+        switch ($PSCmdlet.ParameterSetName) {
+            'BySearch' { 
+                $url = "https://api.alero.io/v2-edge/users/?limit=$Limit&name=$Name&offset=$Offset"
+            }
+            'ById' {
+                $url = "https://api.alero.io/v2-edge/users/$UserId"
+            }
+            Default {}
+        }
         $result = Invoke-RestMethod -Method Get -Uri $url -Authentication Bearer -Token $Authn
     }
     
