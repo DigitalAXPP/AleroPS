a5711a85f5c0dc02674514625d73e8c8002ec8b1
DigitalAXPP <a.piepenhagen@gmx.net>
2020-12-05 23:28:09 +0100

    New function: New-AleroVendorInvitation

---
+++ Functions/Invitations/New-VendorInvitation.ps1
@@ -0,0 +1,32 @@
+function New-AleroVendorInvitation {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName,
+            HelpMessage='Token to authenticate to Alero.'
+        )]
+        [System.Security.SecureString]$Authn,
+
+        [Parameter(
+            Mandatory,
+            HelpMessage='Enter all properties of the Vendor Invitation Request'
+        )]
+        [HashTable]$InvitationRequest
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        $url = "https://api.alero.io/v2-edge/invitations/vendor-invitations"
+        $body = $InvitationRequest | ConvertTo-Json -Depth 3
+        $result = Invoke-RestMethod -Method Post -Uri $url -Body $body -Authentication Bearer -Token $Authn -ContentType 'application/json'
+    }
+    
+    end {
+        Write-Output -InputObject $result
+        Remove-Variable -Name result
+    }
+}
