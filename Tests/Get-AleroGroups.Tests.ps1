25fb381c073d94c5b076ad924815d119da462252
DigitalAXPP <a.piepenhagen@gmx.net>
2021-01-03 23:57:49 +0100

    Completing Pester tests for Get-AleroGroups

--- Tests/Get-AleroGroups.Tests.ps1
+++ Tests/Get-AleroGroups.Tests.ps1
@@ -12,21 +12,57 @@ Describe "Get-AleroGroups" {
     }
     Context "Verifying parameters" {
         $mandatoryParameter = @(
-            @{ Parameter = "Authn" }
-            @{ Parameter = "Search" }
+            @{ 
+                Parameter = "Authn"
+                ParameterSet = "BySearch"
+            }
+            @{ 
+                Parameter = "Search"
+                ParameterSet = "BySearch"
+            }
+            @{ 
+                Parameter = "Authn"
+                ParameterSet = "ByGroupId"
+            }
+            @{ 
+                Parameter = "GroupId"
+                ParameterSet = "ByGroupId"
+            }
         )
-        It "<Parameter> is mandatory" -TestCases $mandatoryParameter {
-            param($Parameter)
+        It "<Parameter> is mandatory for ParameterSet <ParameterSet>" -TestCases $mandatoryParameter {
+            param($Parameter, $ParameterSet)
             $functionMeta = Get-Command -Name Get-AleroGroups
-            $functionMeta.Parameters[$Parameter].Attributes.Mandatory | Should -BeTrue
+            $functionMeta.Parameters[$Parameter].ParameterSets[$ParameterSet].IsMandatory | Should -BeTrue
         }
     }
     Context "Verify the output" {
         BeforeEach {
             $auth = New-AleroToken -Path $configPath -Datacenter $configFile.Datacenter -TenantID $configFile.TenantID -AsSecureString
         }
-        It "Returning all Alero groups" {
-            $output = Get-AleroGroups -Authn $auth -Search "*"
+        It "Returning Alero groups by query string" {
+            $output = Get-AleroGroups -Authn $auth -Search "GRP"
+            $output | Should -Not -BeNullOrEmpty
+            $output | Should -BeOfType [PSCustomObject]
+            $output.groups.Count | Should -BeGreaterThan 5
+        }
+        It "Returning group by description" {
+            $output = Get-AleroGroups -Authn $auth -Search "Alero" -SearchIn DESCRIPTION
+            $output.groups.Count | Should -Be 1
+        }
+        It "Returning only the last three groups" {
+            $output = Get-AleroGroups -Authn $auth -Search "PwSh" -Offset 2
+            $output.groups.Count | Should -Be 3
+        }
+        It "Returning a limit number of groups" {
+            $output = Get-AleroGroups -Authn $auth -Search "PwSh" -Limit 2
+            $output.groups.Count | Should -Be 2
+        }
+        It "Returning groups by combining Offset and Limit" {
+            $output = Get-AleroGroups -Authn $auth -Search "PwSh" -Limit 1 -Offset 2
+            $output.groups.Count | Should -Be 1
+        }
+        It "Returning group by GroupId" {
+            $output = Get-AleroGroups -Authn $auth -GroupId $configFile.GroupID
             $output | Should -Not -BeNullOrEmpty
             $output | Should -BeOfType [PSCustomObject]
         }
